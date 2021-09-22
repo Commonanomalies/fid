654f780c7dd896d56d624f07a57971cc186290cd
Erin Hiles <erin.hiels527@gmail.com>
2021-09-21 20:53:47 -0400

    Update JS

--- blackgoose/js/blackgoosebistro.js
+++ blackgoose/js/blackgoosebistro.js
@@ -11,5 +11,28 @@ function title(){
 }
 
 function menu(){
-	
+	var request = new XMLHttpRequest();
+	request.open('GET', 'include/topMenu.html', true);
+	request.onload = function(){
+		if(request.status >= 200 && request.status < 400){
+			var header = request.responseText;
+			document.getElementById('topMenu').innerHTML = header;
+		}
+	};
+	request.send();
+}
+
+function addFromInclude(objStrName){
+	const str = "include/";
+	const strEnd = ".html";
+	fullStr = concat(str, objStrName, strEnd);
+var request = new XMLHttpRequest();
+	request.open('GET', fullStr, true);
+	request.onload = function(){
+		if(request.status >= 200 && request.status < 400){
+			var header = request.responseText;
+			document.getElementById(concat(objStrName, strEnd)).innerHTML = header;
+		}
+	};
+	request.send();
 }

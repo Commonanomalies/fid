b800a313338c12636feef7a10de3f3ca8aa17831
Erin Hiles <erin.hiels527@gmail.com>
2021-09-21 20:34:07 -0400

    Test

--- blackgoose/js/blackgoosebistro.js
+++ blackgoose/js/blackgoosebistro.js
@@ -1,6 +1,6 @@
 function title(){
 	var request = new XMLHttpRequest();
-	request.open('GET', '../include/logo.html', true);
+	request.open('GET', '/blackgoose/include/logo.html', true);
 	request.onload = function(){
 		if(request.status >= 200 && request.status < 400){
 			var header = request.responseText;

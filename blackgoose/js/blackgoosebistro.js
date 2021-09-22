690d7b1b093f540d5566eeaeec8b8ec9deeb2a71
Erin Hiles <erin.hiels527@gmail.com>
2021-09-21 20:57:28 -0400

    Cleaned UP JS testing Data. Added Top Menu

--- blackgoose/js/blackgoosebistro.js
+++ blackgoose/js/blackgoosebistro.js
@@ -1,32 +1,9 @@
-function title(){
-	var request = new XMLHttpRequest();
-	request.open('GET', 'include/logo.html', true);
-	request.onload = function(){
-		if(request.status >= 200 && request.status < 400){
-			var header = request.responseText;
-			document.getElementById('logo').innerHTML = header;
-		}
-	};
-	request.send();
-}
-
-function menu(){
-	var request = new XMLHttpRequest();
-	request.open('GET', 'include/topMenu.html', true);
-	request.onload = function(){
-		if(request.status >= 200 && request.status < 400){
-			var header = request.responseText;
-			document.getElementById('topMenu').innerHTML = header;
-		}
-	};
-	request.send();
-}
 
 function addFromInclude(objStrName){
 	const str = "include/";
 	const strEnd = ".html";
 	fullStr = str.concat(objStrName, strEnd);
-var request = new XMLHttpRequest();
+	var request = new XMLHttpRequest();
 	request.open('GET', fullStr, true);
 	request.onload = function(){
 		if(request.status >= 200 && request.status < 400){
